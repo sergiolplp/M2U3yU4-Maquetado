1118860ecc256fe2a4f6e5b7033fdf31d8859bcc
Sergio Sainz <sergiosainz.proyectos@gmail.com>
2022-06-23 19:54:46 -0300

    arreglos

---
+++ css/script.js
@@ -0,0 +1,8 @@
+var abrirpopup = document.getElementById('btn-warning'); //Acceso a boton "mas info"
+var mostrar = document.getElementById('mostrar'); //acceso al fondo del formulario
+var popup = document.getElementById('mostrar'); //acceso al formulario
+var cerrarpopup = document.getElementById('boton-cerrar-popup'); //acceso boton de cierre de formulario
+
+abrirpopup.addEventListener('click', function(){mostrar.classList.add(active);});
+
+cerrarpopup.addEventListener('click', function(){mostrar.classList.remove(active);});
